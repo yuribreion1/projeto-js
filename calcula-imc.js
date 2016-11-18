bcacf0574416066af89ff4e6f8399a65e75b2632
yuribreion1 <yuribreion@gmail.com>
2016-11-18 16:57:08 -0200

    Refatorado código
    Acerto feito na variavel imcDoPaciente

--- calcula-imc.js
+++ calcula-imc.js
@@ -11,16 +11,16 @@ for(var posicaoDoTrAtual = 0; posicaoDoTrAtual <= nossosTrs.length -1; posicaoDo
 	peso : pesoTd.textContent,
 	altura : alturaTd.textContent };
 
-	var imc = calculaImc(paciente); //calculamos o imc usando o paciente como parametro e gravando dentro de imc
+	var imcDoPaciente = calculaImc(paciente); //calculamos o imc usando o paciente como parametro e gravando dentro de imc
 
 	var imcTd = trAtual.getElementsByClassName("info-imc")[0];
-	imcTd.textContent = imc; //imprimindo o valor de imc nas TD's
+	imcTd.textContent = imcDoPaciente; //imprimindo o valor de imc nas TD's
 
 }
 function calculaImc(paciente){ //criando uma função que tem somente o calculo do IMC
 		if(paciente.altura != 0){
 			var imcDoPaciente = paciente.peso / (paciente.altura * paciente.altura);
-			return imcDoPaciente;
+			return imcDoPaciente; //temos que retornar um valor para poder usar este valor
 		} else{
 			console.log("Não posso executar uma divisão por 0!");
 		}
