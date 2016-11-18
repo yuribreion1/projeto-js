9c0af185dca5864b4658be199e632e409487c5b8
yuribreion1 <yuribreion@gmail.com>
2016-11-18 16:20:50 -0200

    Nova logica
    Coloquei nova logica baseada na aula de funcões.

--- calcula-imc.js
+++ calcula-imc.js
@@ -5,21 +5,25 @@ for(var posicaoDoTrAtual = 0; posicaoDoTrAtual <= nossosTrs.length -1; posicaoDo
 	var nomeTd = trAtual.getElementsByClassName("info-nome")[0]; //pega nome da posicao atual
 	var pesoTd = trAtual.getElementsByClassName("info-peso")[0]; //pega peso na posicao atual
 	var alturaTd = trAtual.getElementsByClassName("info-altura")[0]; //pega peso na posicao atual
-	var imcTd = trAtual.getElementsByClassName("info-imc")[0];
 
 	var paciente = {
 	nome : nomeTd.textContent,
 	peso : pesoTd.textContent,
-	altura : alturaTd.textContent
-}
+	altura : alturaTd.textContent };
 
-	if(paciente.altura != 0){
-		var imcDoPaciente = paciente.peso / (paciente.altura * paciente.altura);
-		imcTd.textContent = imcDoPaciente;
-		console.log(imcDoPaciente);
-	} else{
-		console.log("Não posso executar uma divisão por 0!");
-	}
+	var imc = calculaImc(paciente); //calculamos o imc usando o paciente como parametro e gravando dentro de imc
+
+	var imcTd = trAtual.getElementsByClassName("info-imc")[0];
+	imcTd.textContent = imc; //imprimindo o valor de imc nas TD's
+
 }
+function calculaImc(paciente){ //criando uma função que tem somente o calculo do IMC
+		if(paciente.altura != 0){
+			var imcDoPaciente = paciente.peso / (paciente.altura * paciente.altura);
+			return imcDoPaciente;
+		} else{
+			console.log("Não posso executar uma divisão por 0!");
+		}
+	}
 
 
